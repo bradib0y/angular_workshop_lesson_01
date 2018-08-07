c83a313fa06b36240c466c97e77c1ac7a2ad06ce
Balazs Pukli <balazsptm@gmail.com>
2018-08-07 23:35:59 +0200

    added server component

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "../../../node_modules/@angular/core";
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html',
+    styleUrls: ['./server.component.css']
+})
+export class ServerComponent {
+
+}
