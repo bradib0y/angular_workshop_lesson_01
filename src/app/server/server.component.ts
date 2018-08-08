504d94e67d0e9582a329b25904446602cbb66464
Balazs Pukli <balazsptm@gmail.com>
2018-08-08 06:16:30 +0200

    ngstyle, ngclass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,6 +6,29 @@ import { Component } from "../../../node_modules/@angular/core";
     styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
-    serverId: number = 1 + Math.floor((Math.random()*55));
-    serverStatus: string = "Running";
+    serverId: number = 11 + Math.floor((Math.random()*55));
+    serverStatus: boolean = this.randomBoolean();
+
+    randomBoolean(){
+        if(Math.random()>0.5){return true;}
+        else{return false;}
+    }
+
+    getStatusStyle(){
+        if(this.serverStatus){
+            return "badge-success";
+        }
+        else{
+            return "badge-danger";
+        }
+    }
+
+    getStatusText(){
+        if(this.serverStatus){
+            return "ON";
+        }
+        else{
+            return "OFF";
+        }
+    }
 }
